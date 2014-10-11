a51453dbef2c3e6fde5a888b122bd631b3dd5d96
venkatsalem <venkatsalem@gmail.com>
2014-10-11 16:39:36 -0400

    made a few minor fixes

--- wicket-har-load-test/src/main/java/com/webmoli/loadtest/LoadRunnerMain.java
+++ wicket-har-load-test/src/main/java/com/webmoli/loadtest/LoadRunnerMain.java
@@ -9,16 +9,20 @@ import org.apache.commons.lang3.math.NumberUtils;
 public class LoadRunnerMain {
 
 	public static void main(String[] args) {
-		try {
-			String harFile = args[0];
-			int numberOfThread = NumberUtils.toInt(args[1]);
-			String domain = args[2];
+		if (args == null || args.length < 3) {
+			System.err.println("This program require 3 arguments. <har-file-path> <nunber-of-thread> <domain-name>");
+		} else {
+			try {
+				String harFile = args[0];
+				int numberOfThread = NumberUtils.toInt(args[1]);
+				String domain = args[2];
 
-			Collection<String> extensionToIgnore = new ArrayList<String>(Arrays.asList(new String[] { "png", "css", "js", "gif", "m4v" }));
-			LoadRunnerEngine engine = new LoadRunnerEngine(harFile, numberOfThread, domain, extensionToIgnore);
-			engine.startTest();
-		} catch (Exception exp) {
-			exp.printStackTrace();
+				Collection<String> extensionToIgnore = new ArrayList<String>(Arrays.asList(new String[] { "png", "css", "js", "gif", "m4v" }));
+				LoadRunnerEngine engine = new LoadRunnerEngine(harFile, numberOfThread, domain, extensionToIgnore);
+				engine.startTest();
+			} catch (Exception exp) {
+				exp.printStackTrace();
+			}
 		}
 	}
 }
